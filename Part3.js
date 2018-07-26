44b2fa79e8606e8617821c438880f029cecef7a3
derobie <derobie@loyola.edu>
2018-07-25 20:24:41 -0400

    update2

--- Part3.js
+++ Part3.js
@@ -1,38 +1,68 @@
 //Javascript for part3
+//function that is called onload of the body in Part3.html
 function go() {
+    //gets the first body tag
     var body = document.getElementsByTagName("body")[0];
+    //creates the div element
     var divTag = document.createElement("div");
     
+    //creates the selectbox
     var selectBox = document.createElement("select");
-    
-    $(document).ready(function(){
-        //divTag.innerHTML = "test2"
-        
-    })
 
+    //Stores the json information
+    var a = [];
+    
+    //stores the animal information into an array
+    var animalList = [];
+    
+    //creates the xml request
     var xhr = new XMLHttpRequest();
     xhr.open('GET','http://localhost:3000/',true);
     
-    
+    //on state change call the function
     xhr.onreadystatechange = processRequest;
    
     function processRequest(e){
         if(xhr.readyState == 4){
-            divTag.innerHTML = "test2";
-            alert(xhr.status);
+            //gets the json object stores it in a
+            a = JSON.parse(xhr.responseText);
+            //Fills the list of animals with information from the json
+            for(var i=0; i<a.length;i++){
+               
+                animalList.push(new animal(a[i].Animal,a[i].Description));
+            }
             
+            //populates the select box
+            for(var j=0; j<animalList.length;j++){
+                var animalName = animalList[j].name;
+                selectBox.options[selectBox.options.length]= new Option(animalName,j);
+                
+            }
+            
+            //adds an event listener to change the discription
+            selectBox.addEventListener(
+                'change',
+                function(){divTag.innerHTML=animalList[selectBox.selectedIndex].discription;},
+                false
+            );
+            
+            //sets the default value
+            divTag.innerHTML=animalList[selectBox.selectedIndex].discription;
         }
     }
+    //sends the request
      xhr.send();
-     
-     
-    body.appendChild(divTag);
+ 
+    //adds the selectbox and div(for the description) to the body
     body.appendChild(selectBox);
+    body.appendChild(divTag);
     
 }
 
 
-
+//Function to create the animal
+//assigns a newname(sting) to name
+//assigns a newDiscription(string) to discription
 function animal(newName,newDiscription)
 {
     this.name=newName;
--- Total.js
+++ Total.js
@@ -32,10 +32,21 @@ var con = mysql.createConnection({
     });
 //Creates the server
 http.createServer(function (req, res) {
-    var body = '<html><head><title> this is a title </title></head><body><p>This is also a test</p></body></html>';
-    res.writeHead(200,{"Content-Type":"text/html"});
-    res.write(body);
-    res.end();
+    var q = url.parse(req.url,true);
+    var filename = "."+q.pathname;
+    fs.readFile(filename,function(err,data){
+        if(err){
+            res.writeHead(200,{'Content-Type':'application/json'});
+            return res.end(JSON.stringify(jsonArray));
+            /*
+            res.writeHead(404,{'Content-Type':'text/html'});
+            return res.end("404 Not Found");
+            */
+        }
+        res.writeHead(200,{'Content-Type':'text/html'});
+        res.write(data);
+        return res.end();
+    });
    
     
    
